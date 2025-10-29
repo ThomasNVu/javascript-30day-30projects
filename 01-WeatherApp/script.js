4c900399a9257d6ec1240723bdd9d85bb4de7980
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-10-29 19:18:16 -0400

    Add changes to Temp Humidity Location and Image

--- 01-WeatherApp/script.js
+++ 01-WeatherApp/script.js
@@ -1,9 +1,40 @@
 const apiKey = "f452b0b6559d4478fbb14300a9e447b0";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&q=atlanta";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   let data = await response.json();
 
   console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+  document.querySelector(".humidity").innerHTML =
+    Math.round(data.main.humidity) + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main == "Snow") {
+    weatherIcon.src = "images/snow.png";
+  }
 }
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+// checkWeather("New York");
