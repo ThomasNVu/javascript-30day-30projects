9d3e7e892cd6ac1b2fce1df0f9f6e8da458520f1
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-10-29 19:26:39 -0400

    Display error message for invalid city

--- 01-WeatherApp/script.js
+++ 01-WeatherApp/script.js
@@ -10,6 +10,28 @@ async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   let data = await response.json();
 
+  if (response.status == 404) {
+    document.querySelectorAll(".error").style.display = "block";
+    document.querySelectorAll(".weather").style.display = "None";
+  } else {
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.png";
+    }
+
+    document.querySelectorAll(".weather").style.display = "block";
+    document.querySelectorAll(".error").style.display = "None";
+  }
+
   console.log(data);
 
   document.querySelector(".city").innerHTML = data.name;
@@ -17,20 +39,6 @@ async function checkWeather(city) {
   document.querySelector(".humidity").innerHTML =
     Math.round(data.main.humidity) + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
-
-  if (data.weather[0].main == "Clouds") {
-    weatherIcon.src = "images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "images/clear.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    weatherIcon.src = "images/drizzle.png";
-  } else if (data.weather[0].main == "Mist") {
-    weatherIcon.src = "images/mist.png";
-  } else if (data.weather[0].main == "Rain") {
-    weatherIcon.src = "images/rain.png";
-  } else if (data.weather[0].main == "Snow") {
-    weatherIcon.src = "images/snow.png";
-  }
 }
 
 searchBtn.addEventListener("click", () => {
